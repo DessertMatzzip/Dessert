4681c77b46b290fe866a5df355a48b60f3668e2b
Lee Jeon Hyeong <jhlove0490@gmail.com>
2018-08-10 15:16:36 +0900

    스토리 보드 10page 설정창 추가

---
+++ app/src/main/java/com/example/jh/mydessert/SettingNoticeActivity.java
@@ -0,0 +1,19 @@
+package com.example.jh.mydessert;
+
+import android.content.SharedPreferences;
+import android.preference.ListPreference;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingNoticeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_notice);
+
+
+
+    }
+}
